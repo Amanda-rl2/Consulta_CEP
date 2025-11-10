2bf3e7d2b9e4e4e0dd0147fd80633dc2e25d9831
Amanda-rl2 <rodriguesamanda264@gmail.com>
2025-11-10 18:27:41 -0300

    Troquei a fonte adicionando o google fontes

--- lib/view/consulta_page.dart
+++ lib/view/consulta_page.dart
@@ -1,6 +1,7 @@
 import 'package:consulta_cep/controller/consulta_controller.dart';
 import 'package:consulta_cep/widgets/card_information_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ConsultaPage extends StatefulWidget {
   const ConsultaPage({super.key});
@@ -15,43 +16,62 @@ class _ConsultaPageState extends State<ConsultaPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Consulta'), centerTitle: true),
+      appBar: AppBar(
+        title: Text(
+          'Consulta',
+          style: GoogleFonts.kodchasan(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: Colors.green.shade400,
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: TextFormField(
-                    decoration: const InputDecoration(labelText: 'CEP'),
-                    controller: controller.cepController,
+            Container(
+              color: Color(0xffe0f2dc),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'CEP'),
+                      controller: controller.cepController,
+                    ),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      await controller.getConsulta();
-                      setState(() {});
-                    },
-                    child: const Text('Consultar'),
+                  Expanded(
+                    flex: 1,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await controller.getConsulta();
+                        setState(() {});
+                      },
+                      child: const Text('Consultar'),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
 
             const SizedBox(height: 20),
             CardInformationWidget(
               title: 'Logradouro',
               value: controller.consulta.logradouro,
+              color: Color(0xffe1e9d5),
             ),
             CardInformationWidget(
               title: 'Bairro',
               value: controller.consulta.bairro,
+              color: Color(0xffe1e9d5),
             ),
-            CardInformationWidget(title: 'UF', value: controller.consulta.uf),
+            CardInformationWidget(
+              title: 'UF',
+              value: controller.consulta.uf,
+              color: Color(0xffe1e9d5),
+            ),
           ],
         ),
       ),
--- lib/widgets/card_information_widget.dart
+++ lib/widgets/card_information_widget.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CardInformationWidget extends StatelessWidget {
   final String title;
   final String value;
+  final Color? color;
 
-  const CardInformationWidget({super.key, required this.title, required this.value});
+  const CardInformationWidget({super.key, required this.title, required this.value, this.color});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: Card(
+        color: color,
         child: Padding(
           padding: const EdgeInsets.all(12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
-            children: [Text(title), Text(value)],
+            children: [Text(title, style: GoogleFonts.aBeeZee(),), Text(value)],
           ),
         ),
       ),
